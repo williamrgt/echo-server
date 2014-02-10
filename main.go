345a18ab0dd940e1b3a07c7407fa19a13796bbe2
william roberto <william.roberto.t@gmail.com>
2014-02-10 18:54:30 -0200

    Break line for a better display off the log.

--- main.go
+++ main.go
@@ -8,29 +8,29 @@ import (
 )
 
 func main() {
-	log.Printf("echo-server listening on tcp port 8800") 
-	
-	ln, err := net.Listen("tcp", ":8800") 
-	if err != nil { 
-		log.Fatalf("listen error, err=%s", err) 
-	} 
-	
-	accepted := 0 
-	for { 
-			conn, err := ln.Accept() 
-			if err != nil { 
-				log.Fatalf("accept error, err=%s", err) 
-			} 
-			accepted++ 
-			go handleConnection(conn) 
-			log.Printf("connection accepted %d", accepted) 
-	} 
-} 
+	log.Printf("echo-server listening on tcp port 8800")
 
-func handleConnection(conn net.Conn) { 
+	ln, err := net.Listen("tcp", ":8800")
+	if err != nil {
+		log.Fatalf("listen error, err=%s", err)
+	}
+
+	accepted := 0
+	for {
+			conn, err := ln.Accept()
+			if err != nil {
+				log.Fatalf("accept error, err=%s", err)
+			}
+			accepted++
+			go handleConnection(conn)
+			log.Printf("connection accepted %d", accepted)
+	}
+}
+
+func handleConnection(conn net.Conn) {
 	bufr := bufio.NewReader(conn)
 	buf := make([]byte, 1024)
-	
+
 	for {
 		readBytes, err := bufr.Read(buf)
 		if err != nil {
@@ -39,6 +39,6 @@ func handleConnection(conn net.Conn) {
 			return
 		}
 		log.Printf("<->\n%s", hex.Dump(buf[:readBytes]))
-		conn.Write([]byte("CLOUDWALK "+string(buf[:readBytes])))
+		conn.Write([]byte(string(buf[:readBytes])))
 	}
 }
